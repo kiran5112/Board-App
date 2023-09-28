9614a3c596663be45d8c68687c1ef07bbf789cb7
kiran5112 <kbijarane@gmail.com>
2023-09-28 14:28:58 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+import KanbanBoard from './KanbanBoard'
+function App() {
+
+
+  return (
+    <div>
+
+<KanbanBoard/>
+    </div>
+  )
+}
+export default App
